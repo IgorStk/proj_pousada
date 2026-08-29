ad065a32fe3117c81b09a157dc0e44d5b05eb39b
IgorStk <igor29306@gmail.com>
2026-08-29 19:48:13 -0300

    coisas

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000/api",
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 api.interceptors.request.use((config) => {
